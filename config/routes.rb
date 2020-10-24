b2c2a73cda51f3fccdfd7832ca01f7a33b6e2c6d
jacobrees <jacob.rees97@yahoo.com>
2020-10-24 14:27:31 +0100

    Creat users controller, create signup path and add this to the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  match '/',    to: 'static_pages#home',    via: 'get'
+  get 'users/new'
+  match '/',        to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
