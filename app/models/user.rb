78b8131cf3c76a46825150b1565ebec9988eb46b
jacobrees <jacob.rees97@yahoo.com>
2020-10-27 12:30:01 +0000

    Create user edit form where user can update their own details

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                       uniqueness: {case_sensitive: false}
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, allow_blank: true
     has_secure_password
 
     def User.digest(string)
