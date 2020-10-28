40ed8f42548bd9138f7056104050553ddeefdd58
jacobrees <jacob.rees97@yahoo.com>
2020-10-28 14:34:20 +0000

    Create activation mailer and new columns in user database to state whether a user has activated their account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
+    before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50, minimum: 5 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
@@ -22,12 +23,20 @@ class User < ApplicationRecord
       update_attribute(:remember_digest, User.digest(remember_token))
     end
 
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
     end
     
     def forget
       update_attribute(:remember_digest, nil)
+    end
+
+    private
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
