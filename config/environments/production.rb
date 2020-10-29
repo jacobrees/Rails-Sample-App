31e8679040c3322ae2722c179561158c3272c2e3
jacobrees <jacob.rees97@yahoo.com>
2020-10-29 12:14:45 +0000

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://fast-sands-44426.herokuapp.com'
+  host = 'https://pure-tundra-44420.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
